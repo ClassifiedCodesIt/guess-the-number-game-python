2a5c04cc77586abe39dbdd57d351f0205bfbbf9f
ClassifiedCodesIt <148991833+ClassifiedCodesIt@users.noreply.github.com>
2025-01-07 18:47:56 +0000

    Created Initial Project

---
+++ art.py
@@ -0,0 +1,26 @@
+logo = """
+  ,ad8888ba,  88                                88    ad88 88                     88  
+ d8"'    `"8b 88                                ""   d8"   ""                     88  
+d8'           88                                     88                           88  
+88            88 ,adPPYYba, ,adPPYba, ,adPPYba, 88 MM88MMM 88  ,adPPYba,  ,adPPYb,88  
+88            88 ""     `Y8 I8[    "" I8[    "" 88   88    88 a8P_____88 a8"    `Y88  
+Y8,           88 ,adPPPPP88  `"Y8ba,   `"Y8ba,  88   88    88 8PP""""""" 8b       88  
+ Y8a.    .a8P 88 88,    ,88 aa    ]8I aa    ]8I 88   88    88 "8b,   ,aa "8a,   ,d88  
+  `"Y8888Y"'  88 `"8bbdP"Y8 `"YbbdP"' `"YbbdP"' 88   88    88  `"Ybbd8"'  `"8bbdP"Y8
+    ,ad8888ba,                                                        88                      
+   d8"'    `"8b                                                 ,d    88                      
+  d8'                                                           88    88                      
+  88            88       88  ,adPPYba, ,adPPYba, ,adPPYba,    MM88MMM 88,dPPYba,   ,adPPYba,  
+  88      88888 88       88 a8P_____88 I8[    "" I8[    ""      88    88P'    "8a a8P_____88  
+  Y8,        88 88       88 8PP"""""""       `"Y8ba,   `"Y8ba,        88    88       88 8PP"""""""  
+   Y8a.    .a88 "8a,   ,a88 "8b,   ,aa aa    ]8I aa    ]8I      88,   88       88 "8b,   ,aa  
+    `"Y88888P"   `"YbbdP'Y8  `"Ybbd8"' `"YbbdP"' `"YbbdP"'      "Y888 88       88  `"Ybbd8"'
+    888b      88                                88                                 
+    8888b     88                                88                                 
+    88 `8b    88                                88                                 
+    88  `8b   88 88       88 88,dPYba,,adPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+    88   `8b  88 88       88 88P'   "88"    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+    88    `8b 88 88       88 88      88      88 88       d8 8PP""""""" 88          
+    88     `8888 "8a,   ,a88 88      88      88 88b,   ,a8" "8b,   ,aa 88          
+    88      `888  `"YbbdP'Y8 88      88      88 8Y"Ybbd8"'   `"Ybbd8"' 88 
+"""
